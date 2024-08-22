1d0b718e7128aadcc19ff4ba7e629c5c089b78be
Charlie Campbell <chacha9669@hotmail.com>
2024-08-22 16:15:38 +1000

    week 4 libraries

---
+++ Week 4 - Libraries/MathsFuncsLib/MathsFuncsLib.cpp
@@ -0,0 +1,11 @@
+// MathsFuncsLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Utility.h"
+
+// TODO: This is an example of a library function
+void fnMathsFuncsLib()
+{
+}
---
+++ Week 4 - Libraries/MathsFuncsLib/Utility.cpp
@@ -0,0 +1,7 @@
+#include "Utility.h";
+#include "pch.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
